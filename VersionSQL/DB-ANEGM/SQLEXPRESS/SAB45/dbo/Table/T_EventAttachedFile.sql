e5a7acad0efde5aeaa3fe10bfe040e2bce8875df
Amr Negm <Amr.Negm@g2k-group.com>
2022-01-31 12:05:04 +0200

    DISABLE TRIGGER [NewEventAttachement]

--- VersionSQL/DB-ANEGM/SQLEXPRESS/SAB45/dbo/Table/T_EventAttachedFile.sql
+++ VersionSQL/DB-ANEGM/SQLEXPRESS/SAB45/dbo/Table/T_EventAttachedFile.sql
@@ -15,12 +15,6 @@ CREATE TABLE [dbo].[T_EventAttachedFile](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-ALTER TABLE [dbo].[T_EventAttachedFile]  WITH CHECK ADD  CONSTRAINT [FK_EventAttachedFile] FOREIGN KEY([EventID], [EventSiteID])
-REFERENCES [dbo].[T_Events] ([EventID], [EventSiteID])
-ALTER TABLE [dbo].[T_EventAttachedFile] CHECK CONSTRAINT [FK_EventAttachedFile]
-ALTER TABLE [dbo].[T_EventAttachedFile]  WITH CHECK ADD  CONSTRAINT [FK_EventAttachedType] FOREIGN KEY([EventAttachedTypeID])
-REFERENCES [dbo].[LT_AlarmAttachedType] ([AlarmAttachedTypeID])
-ALTER TABLE [dbo].[T_EventAttachedFile] CHECK CONSTRAINT [FK_EventAttachedType]
 SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 CREATE TRIGGER [dbo].[DeleteEventAttachement] ON  [dbo].[T_EventAttachedFile]
@@ -29,6 +23,20 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 	Delete from SAB45_EVENTS.dbo.[T_EventAttachedFile] where EventAttachedFileID = (select EventAttachedFileID from deleted)
+END
+
+ALTER TABLE [dbo].[T_EventAttachedFile] ENABLE TRIGGER [DeleteEventAttachement]
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TRIGGER [dbo].[NewEventAttachement] ON  [dbo].[T_EventAttachedFile]
+   AFTER INSERT
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	insert into SAB45_EVENTS.dbo.[T_EventAttachedFile] select * from inserted i
+END
+
+ALTER TABLE [dbo].[T_EventAttachedFile] DISABLE TRIGGER [NewEventAttachement]Delete from SAB45_EVENTS.dbo.[T_EventAttachedFile] where EventAttachedFileID = (select EventAttachedFileID from deleted)
 END
 
 ALTER TABLE [dbo].[T_EventAttachedFile] ENABLE TRIGGER [DeleteEventAttachement]
